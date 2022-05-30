86e47e594af3fef082da26e46557f278bbc8d908
Isai <velascohernandes.2017@gmail.com>
2022-05-30 13:16:44 -0500

    Update Printer View

--- RafaelReyesSpindola/Controllers/PrintersController.cs
+++ RafaelReyesSpindola/Controllers/PrintersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using RafaelReyesSpindola.Models;
 
 namespace RafaelReyesSpindola.Controllers
 {
+    [Authorize(Roles = "Administrador, Cajero")]
     public class PrintersController : Controller
     {
         private readonly SchoolContext _context;
