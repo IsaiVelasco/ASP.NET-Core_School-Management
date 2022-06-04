4b25db9a452b4e96164689a70455ec3efb4a2ec4
Isai <velascohernandes.2017@gmail.com>
2022-06-03 23:30:40 -0500

    Update Script

--- RafaelReyesSpindola/wwwroot/assets/demo/chart-area-demo.js
+++ RafaelReyesSpindola/wwwroot/assets/demo/chart-area-demo.js
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 18451],
     }],
   },
   options: {
--- RafaelReyesSpindola/wwwroot/assets/demo/chart-bar-demo.js
+++ RafaelReyesSpindola/wwwroot/assets/demo/chart-bar-demo.js
@@ -12,7 +12,7 @@ var myLineChart = new Chart(ctx, {
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [4215, 5312, 10000, 7841, 9821, 10984],
     }],
   },
   options: {
